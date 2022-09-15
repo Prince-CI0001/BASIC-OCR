1d2ae1f85c30843b9ce2e17b6efbabc725dd5cb7
Prince-CI0001 <prince@caizin.com>
2022-09-15 11:00:05 +0530

    update

--- app.js
+++ app.js
@@ -1,48 +1,70 @@
 "use strict";
 let character = document.querySelectorAll("input");
-let inputString = "";
 const obj = {
     " _ | ||_|   ": "0",
+    "    _ | ||_|": "0",
+    "|  |  |     ": "1",
+    " |  |  |    ": "1",
+    "  |  |  |   ": "1",
+    "|  |        ": "1",
+    " |  |       ": "1",
+    "  |  |      ": "1",
+    "   |  |     ": "1",
+    "    |  |    ": "1",
     "     |  |   ": "1",
-    " _  _||_    ": "2",
+    "___|_||__   ": "2",
     " _  _| _|   ": "3",
-    "   |_|  |   ": "4",
+    "|  ||_|  |  ": "4",
     " _ |_  _|   ": "5",
     " _ |_ |_|   ": "6",
     " _   |  |   ": "7",
     " _ |_||_|   ": "8",
     " _ |_| _|   ": "9",
     " _ |_|| |   ": "A",
+    "    _ |_|| |": "A",
     "    _ |_||_|": "B",
     "    _ |  |_ ": "C",
+    " _ |  |_    ": "C",
     " _ | || ||_|": "D",
     "    _ |_ |_ ": "E",
-    "    _ |_ |  ": "F",
-    "    _ |_ |_|": "G",
-    "      |_|| |": "H",
-    "       |  | ": "I",
-    "       | _| ": "J",
-    "      |/ |\\ ": "K",
-    "      |  |_ ": "L",
-    "": "M",
-    " ": "N",
-    "    _ | ||_|": "O",
+    " _ |_ |_    ": "E",
+    "___|__|  |  ": "F",
+    "|_ |__|_|   ": "G",
+    "   |_ |__|_|": "G",
+    "| ||_ || |   ": "H",
+    "   | ||_ || |": "H",
+    "___ | ___   ": "I",
+    "   ___ | ___": "I",
+    "___ | \\ |    ": "J",
+    "   ___ | \\ | ": "J",
+    " |  |/ |\\ | ": "K",
+    "|  |/ |\\ |  ": "K",
+    "|  |  |__   ": "L",
+    "   |  |  |__": "L",
+    "   \\ /|_|| |": "M",
+    "\\ /|_|| |   ": "M",
+    "\\ /|_|| || |": "M",
+    "| ||\\|| \\   ": "N",
+    "   | ||\\|| \\": "N",
+    " _ | || | _ ": "O",
     "    _ |_||  ": "P",
     "    _ | ||\\|": "Q",
     " _ |_|| \\   ": "R",
     "    _ |_  _|": "S",
     "       _  | ": "T",
     "      | ||_|": "u",
-    "   ": "V",
+    "\\ / _       ": "V",
+    "   \\ / _    ": "V",
     "    ": "w",
     "\\ / | / \\   ": "X",
     "\\ / \\ /     ": "y",
-    "   _   / /_ ": "Z",
+    "___ / /__   ": "Z",
+    "   ___ / /__": "Z",
 };
 window.onload = () => {
     character.forEach(ele => {
         ele.addEventListener("keypress", (event) => {
-            let str = "|_|?";
+            let str = "|_|\\/";
             if (!str.includes(event.key)) {
                 alert("Wrong input");
                 event.preventDefault();
@@ -51,6 +73,7 @@ window.onload = () => {
     });
 };
 function wordCreation(e) {
+    let inputString = "";
     character.forEach(item => {
         if (item.value == "") {
             inputString += " ";
@@ -61,14 +84,12 @@ function wordCreation(e) {
     });
     if (inputString in obj) {
         let outputString = obj[inputString];
-        console.log(outputString);
         document.querySelector("#display").style.display = "block";
         document.querySelector('#display').innerText = outputString;
     }
     else {
         alert("DRAW EXACT WORD");
     }
-    inputString = "";
 }
 function reset() {
     document.querySelector('#display').innerText = "";
--- app.ts
+++ app.ts
@@ -1,53 +1,75 @@
 let character = document.querySelectorAll("input");
-let inputString : string="";
+
 const obj = {
     " _ | ||_|   ": "0",
+    "    _ | ||_|": "0",
+    "|  |  |     ": "1",
+    " |  |  |    ": "1",
+    "  |  |  |   ": "1",
+    "|  |        ": "1",
+    " |  |       ": "1",
+    "  |  |      ": "1",
+    "   |  |     ": "1",
+    "    |  |    ": "1",
     "     |  |   ": "1",
-    " _  _||_    ": "2",
+    "___|_||__   ": "2",
     " _  _| _|   ": "3",
-    "   |_|  |   ": "4",
+    "|  ||_|  |  ": "4",
     " _ |_  _|   ": "5",
     " _ |_ |_|   ": "6",
     " _   |  |   ": "7",
     " _ |_||_|   ": "8",
     " _ |_| _|   ": "9",
     " _ |_|| |   ": "A",
+    "    _ |_|| |": "A",
     "    _ |_||_|": "B",
     "    _ |  |_ ": "C",
+    " _ |  |_    ": "C",
     " _ | || ||_|": "D",
     "    _ |_ |_ ": "E",
-    "    _ |_ |  ": "F",
-    "    _ |_ |_|": "G",
-    "      |_|| |": "H",
-    "       |  | ": "I",
-    "       | _| ": "J",
-    "      |/ |\\ ": "K",
-    "      |  |_ ": "L",
-    "": "M",
-    " ": "N",
-    "    _ | ||_|": "O",
+    " _ |_ |_    ": "E",
+    "___|__|  |  ": "F",
+    "|_ |__|_|   ": "G",
+    "   |_ |__|_|": "G",
+    "| ||_ || |   ": "H",
+    "   | ||_ || |": "H",
+    "___ | ___   ": "I",
+    "   ___ | ___": "I",
+    "___ | \\ |    ": "J",
+    "   ___ | \\ | ": "J",
+    " |  |/ |\\ | ": "K",
+    "|  |/ |\\ |  ": "K",
+    "|  |  |__   ": "L",
+    "   |  |  |__": "L",
+    "   \\ /|_|| |": "M",
+    "\\ /|_|| |   ": "M",
+    "\\ /|_|| || |": "M",
+    "| ||\\|| \\   ": "N",
+    "   | ||\\|| \\": "N",
+    " _ | || | _ ": "O",
     "    _ |_||  ": "P",
     "    _ | ||\\|": "Q",
     " _ |_|| \\   ": "R",
     "    _ |_  _|": "S",
     "       _  | ": "T",
     "      | ||_|": "u",
-    "   ": "V",
+    "\\ / _       ": "V",
+    "   \\ / _    ": "V",
     "    ": "w",
     "\\ / | / \\   ": "X",
     "\\ / \\ /     ": "y",
-    "   _   / /_ ": "Z",
+    "___ / /__   ": "Z",
+    "   ___ / /__": "Z",
 }
-window.onload = ()=>{
+window.onload = () => {
 
-    character.forEach( ele =>{
-        ele.addEventListener("keypress",(event:KeyboardEvent)=>{
-            let str = "|_|?";
-            if(!str.includes(event.key))
-            {
+    character.forEach(ele => {
+        ele.addEventListener("keypress", (event: KeyboardEvent) => {
+            let str = "|_|\\/";
+            if (!str.includes(event.key)) {
                 alert("Wrong input");
                 event.preventDefault();
-            
+
             }
 
         })
@@ -55,38 +77,33 @@ window.onload = ()=>{
 
 }
 
-function wordCreation(e:Event)
-{
-    character.forEach(item =>{
-        
-        if(item.value=="")
-        {
+function wordCreation(e: Event) {
+    let inputString: string = "";
+    character.forEach(item => {
+
+        if (item.value == "") {
             inputString += " ";
         }
-        else{
+        else {
             inputString += item.value;
         }
     })
-    if(inputString in obj)
-    {
+    if (inputString in obj) {
         let outputString = obj[inputString as keyof typeof obj];
-        console.log(outputString);
         (document.querySelector("#display") as HTMLInputElement).style.display = "block";
         (document.querySelector('#display') as HTMLInputElement).innerText = outputString;
     }
-    else{
+    else {
         alert("DRAW EXACT WORD");
     }
-    inputString="";
 }
 
 
 
-function reset()
-{
+function reset() {
     (document.querySelector('#display') as HTMLInputElement).innerText = "";
     (document.querySelector("#display") as HTMLInputElement).style.display = "none";
-    character.forEach(i=>{
-        i.value="";
+    character.forEach(i => {
+        i.value = "";
     })
 }
